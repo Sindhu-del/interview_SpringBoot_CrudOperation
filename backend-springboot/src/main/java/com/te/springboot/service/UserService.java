d0cb42e2da2368110812c2caaae3db8b47642d04
TY_Sindhu_Batch9 <77116979+Sindhu-del@users.noreply.github.com>
2021-07-27 13:25:03 +0530

    Add files via upload

---
+++ backend-springboot/src/main/java/com/te/springboot/service/UserService.java
@@ -0,0 +1,47 @@
+package com.te.springboot.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.te.springboot.model.User;
+import com.te.springboot.repo.UserRepo;
+
+@Service
+public class UserService {
+
+	
+		@Autowired
+		private UserRepo repository;
+
+		public User saveUser(User user) {
+			return repository.save(user);
+		}
+
+		public List<User> saveUsers(List<User> users) {
+			return repository.saveAll(users);
+		}
+
+		public List<User> getUsers() {
+			return repository.findAll();
+		}
+
+		public User  getUserById( Long userID) {
+			return repository.findById(userID).orElse(null);
+		}
+		
+		public String deleteUser(Long userID) {
+			repository.deleteById(userID);
+			return "User removed !! " + userID;
+		}
+
+		public User updateUser(User user) {
+			User existingUser = repository.findById(user.getId()).orElse(null);
+			existingUser.setEmail(user.getEmail());
+		    existingUser.setFirstName(user.getFirstName());
+			return repository.save(existingUser);
+		}
+
+	}
+
